991c569ffa88ed930a4790e9a53c57110f334913
HyoSean <kimhyosun@gmail.com>
2022-02-07 23:59:28 +0900

    map 파일 구조

--- src/components/pages/MapMain.js
+++ src/components/pages/MapMain.js
@@ -1,5 +1,20 @@
+import MapList from '../templates/MapList';
+import MapListDetails from '../templates/MapListDetails';
+import MapListEnter from '../templates/MapListEnter';
+import MapTargetList from '../templates/MapTargetList';
+
 const MapMain = () => {
-	return <div></div>;
+	return (
+		<div className="map_main">
+			<div className="map_kakao"></div>
+
+			<MapList />
+			<MapListDetails />
+			<MapListEnter />
+
+			{<MapTargetList />}
+		</div>
+	);
 };
 
 export default MapMain;
--- src/components/templates/UserMapList.js
+++ src/components/templates/UserMapList.js
@@ -1,15 +1,15 @@
 const UserMapList = () => {
 	return (
 		<div className="user_map_list">
-			<div className="button_box ">
+			{/* <div className="button_box ">
 				<strong>{user.nickname}</strong>
 				<button className="buttons button_right">+</button>
 			</div>
 			<ul>
-				{user.mapList.map((cur) => (
-					<li></li>
+				{user.mapList.map((cur, idx) => (
+					<li key={`${idx}_${new Date().toString()}`}>{cur}</li>
 				))}
-			</ul>
+			</ul> */}
 		</div>
 	);
 };
