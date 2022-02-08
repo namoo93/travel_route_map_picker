fa33b9237e199cacba0ff23b42d24ff1c7594a62
HyoSean <kimhyosun@gmail.com>
2022-02-08 15:33:06 +0900

    이벤트 타겟 이터레이터 객체 분리

--- src/components/templates/UserLogin.js
+++ src/components/templates/UserLogin.js
@@ -2,17 +2,20 @@
 import {useEffect} from 'react';
 
 const UserLogin = ({setLogin, setSignup}) => {
-	useEffect(() => {}, []);
+	useEffect(() => {
+		//서버에서 유저리스트 API 받아온뒤 유저객체값 변수에담기
+	}, []);
 
-	const userInput = (value) => {
-		console.log(value);
+	const userInput = (e) => {
+		const {name, value} = e.target;
+		console.log(name, value);
 	};
 	return (
 		<div className="user_login">
 			<h3>로그인</h3>
 			<div className="input_form border_bottom">
-				<input type="text" className="inputs user_id" onInput={(e) => userInput(e.target.value)} />
-				<input type="text" className="inputs user_password" onInput={(e) => userInput(e.target.value)} />
+				<input type="text" name="user_id" className="inputs user_id" onInput={(e) => userInput(e)} />
+				<input type="text" name="user_password" className="inputs user_password" onInput={(e) => userInput(e)} />
 			</div>
 			<div className="botton_box">
 				<strong>간편 로그인</strong>
