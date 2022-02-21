b200b35335dd31f3f4407280645aff69c3342cc5
HyoSean <kimhyosun@gmail.com>
2022-02-22 01:23:01 +0900

    reset

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 //리덕스 셋팅
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import {rootReducer} from './reducer/index';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './reducer/index';
 import Login from './components/pages/Login';
 import MapMain from './components/pages/MapMain';
-import {useState} from 'react';
 
 const store = createStore(rootReducer);
 
 function App() {
-	const [login, setLogin] = useState(false);
-	return (
-		<Provider store={store}>
-			<div className="App">
-				<Login login={login} setLogin={setLogin} />
-				{login && <MapMain />}
-			</div>
-		</Provider>
-	);
+  const [login, setLogin] = useState(false);
+  useEffect(() => {
+    console.log(login);
+  }, [login]);
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Login login={login} setLogin={setLogin} />
+        {login && <MapMain />}
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
