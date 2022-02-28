6efe7899d5f1908509c4661dbcf548de0c659658
HyoSean <kimhyosun@gmail.com>
2022-02-28 21:57:35 +0900

    맵 initial 팝업

--- frontend/src/components/UI/organisms/Initial.js
+++ frontend/src/components/UI/organisms/Initial.js
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Initial = ({ setModalOpened }) => {
   const [titleValue, setTitleValue] = useState('');
-  const onSubmitTitle = (e) => {
-    e.preventDfault();
-    if (titleValue) {
-      setModalOpened(false);
+  const intialRef = useRef(null);
+  const onSubmitTitle = async (e) => {
+    try {
+      if (titleValue) {
+        setModalOpened(false);
+        //서버에 타이틀값전송 서버 맵리스트에 추가
+        console.log(titleValue);
+      }
+    } catch (error) {
+      console.error(error);
     }
+    e.preventDefault();
   };
 
   const onInputInitial = (e) => {
-    const vlaue = e.target.value;
-    setTitleValue(vlaue);
+    if (e.target.value !== '') {
+      if (e.keyCode === 13) {
+        setTitleValue(intialRef.current.value);
+        console.log(titleValue);
+      }
+    }
+  };
+
+  const onClickInitial = () => {
+    if (intialRef.current.value !== '') {
+      setTitleValue(intialRef.current.value);
+      intialRef.current.value = '';
+      console.log(titleValue);
+    }
   };
   return (
     <div className="initial_box">
       <stron>새로운 장소명을 입력해주세요</stron>
       <form onSubmit={onSubmitTitle}>
         <div className="input_wrap">
-          <input type="text" name="title" autoFocus required onInput={onInputInitial} />
+          <input type="text" name="title" autoFocus required onKeyDown={onInputInitial} ref={intialRef} />
         </div>
       </form>
-      <button type="submit">만들기</button>
+      <button type="submit" onClick={onClickInitial}>
+        만들기
+      </button>
     </div>
   );
 };
