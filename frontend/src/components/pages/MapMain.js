b59faf1d3fb88036a1d890602f385ff0a5ca550d
HyoSean <kimhyosun@gmail.com>
2022-03-04 00:38:34 +0900

    이미지 추가

--- frontend/src/components/pages/MapMain.js
+++ frontend/src/components/pages/MapMain.js
@@ -7,12 +7,16 @@ import ModalPortal from '../templates/ModalPortal';
 import Initial from '../UI/organisms/Initial';
 // import Modal from 'react-awesome-modal';
 
-const options = {
+const mapOptions = {
   //지도를 생성할 때 필요한 기본 옵션
   center: new window.kakao.maps.LatLng(37.56575070868735, 126.97685985928288), //지도의 중심좌표.
   level: 5, //지도의 레벨(확대, 축소 정도)
   disableDoubleClick: true, // 클릭시 확대
   disableDoubleClickZoom: true, // 더블 클릭시 확대
+};
+const markerOptions = {
+  //지도 마커 옵션
+  image: new window.kakao.maps.MarkerImage('/img/Polygon_2.png', new window.kakao.maps.Size(16, 16)),
 };
 
 const MapMain = () => {
@@ -23,8 +27,11 @@ const MapMain = () => {
   const [addrInMap, setAddrInMap] = useState(''); //주소값
   const container = useRef(null); //지도를 담을 영역의 DOM 레퍼런스
 
+  //
+  const customInfoContent = `<div class="info_contents"> dsdsadadasda </div>`;
+
   useEffect(() => {
-    const maps = new window.kakao.maps.Map(container.current, options); //지도 생성 및 객체 리턴
+    const maps = new window.kakao.maps.Map(container.current, mapOptions); //지도 생성 및 객체 리턴
 
     // 주소-좌표 변환 객체를 생성합니다
     const geocoder = new window.kakao.maps.services.Geocoder();
@@ -35,12 +42,13 @@ const MapMain = () => {
     }
 
     // 클릭한 위치를 표시할 마커입니다
-    const marker = new window.kakao.maps.Marker();
-    const infowindow = new window.kakao.maps.InfoWindow();
+    const marker = new window.kakao.maps.Marker(markerOptions);
+    const customInfo = new window.kakao.maps.CustomOverlay({
+      content: customInfoContent,
+    });
 
     //지도 클릭시 마커와 상세 주소출력
     window.kakao.maps.event.addListener(maps, 'click', function (e) {
-      console.log(e.latLng);
       //메모 상세 팝업
       //setMemoAddModal(true);
 
@@ -48,20 +56,18 @@ const MapMain = () => {
         if (status === window.kakao.maps.services.Status.OK) {
           let detailAddr = !!result[0].road_address ? '도로명 주소:' + result[0].road_address.address_name : '';
           detailAddr += ' 지번 주소:' + result[0].address.address_name;
-          console.log(detailAddr);
           //결과 주소값 담기
           setAddrInMap(detailAddr);
 
           //마커위의 인포윈도우
-          const content = '<div class="bAddr">' + detailAddr + '</div>';
-
+          customInfo.setPosition(e.latLng);
+          console.log(e.latLng);
           // 마커를 클릭한 위치에 표시합니다
           marker.setPosition(e.latLng);
           marker.setMap(maps);
 
           // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-          infowindow.setContent(content);
-          infowindow.open(maps, marker);
+          // customInfo.getVisible(true);
         }
       });
 
