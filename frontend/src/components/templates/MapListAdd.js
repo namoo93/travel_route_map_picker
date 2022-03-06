174cccaad1dc6d23e621b9e8f82b6acfde2793de
HyoSean <kimhyosun@gmail.com>
2022-03-06 18:19:53 +0900

    이미지 미리보기

--- frontend/src/components/templates/MapListAdd.js
+++ frontend/src/components/templates/MapListAdd.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //더미 데이터
 import savemap from '../../api/savemap';
 
 const MapListAdd = ({ placeInfo, setMemoAddModal, setMemoContents }) => {
-  const [imgFiles, setImgFiles] = useState([]);
+  const [images, setImages] = useState([]);
   //모든 필수 데이터 입력시 버튼 활성화
   const [disabled, setDisabled] = useState(true);
   //마커의 미리보기 데이터 셋팅 setMemoContents() // 배열안의 객체로 설정
 
+  useEffect(() => {}, [images]);
+
   const onSubmitAddMemo = (e) => {
     e.preventDefault();
+
+    //이미지 서버로 저장
+    const formdata = new FormData();
   };
 
   const onKeyDownTitle = () => {};
@@ -18,8 +23,10 @@ const MapListAdd = ({ placeInfo, setMemoAddModal, setMemoContents }) => {
     const {
       target: { files },
     } = e;
-    console.log(files);
-    setImgFiles(files);
+
+    const filesUrlList = Object.values(files).map((cur) => URL.createObjectURL(cur));
+
+    setImages(filesUrlList);
   };
 
   const onClickAddMemo = () => {
@@ -61,11 +68,18 @@ const MapListAdd = ({ placeInfo, setMemoAddModal, setMemoContents }) => {
             multiple
             onChange={onLoadFiles}
           />
-          <ol className="file_preview"></ol>
+          <ol className="file_preview">
+            {/* 이미지 등록시 미리보기 제공 */}
+            {images.map((cur, idx) => (
+              <li key={idx}>
+                <img src={cur} />
+              </li>
+            ))}
+          </ol>
         </div>
         <div className="input_wrap">
           {/* 글 컨텐츠 & 장소 정보 가져온것 */}
-          <textarea></textarea>
+          <textarea placeholder="하고싶은 말이나 설명을 적어주세요~"></textarea>
           <div>{placeInfo}</div>
         </div>
         <button type="submit" className="buttons w100" onClick={onClickAddMemo} disabled={disabled}>
