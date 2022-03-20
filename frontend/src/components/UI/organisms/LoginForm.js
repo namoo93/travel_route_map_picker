67ebdd1072580f299b5a23d56fcc34600ff98cb5
HyoSean <kimhyosun@gmail.com>
2022-03-21 01:04:05 +0900

    Update LoginForm.js

--- frontend/src/components/UI/organisms/LoginForm.js
+++ frontend/src/components/UI/organisms/LoginForm.js
@@ -1,12 +1,12 @@
 import InputForm from '../molecules/InputForm';
 
 const LoginForm = () => {
-	return (
-		<div className="login_form">
-			<h3>로그인</h3>
-			<InputForm />
-		</div>
-	);
+  return (
+    <div className="login_form">
+      <h3>로그인</h3>
+      <InputForm />
+    </div>
+  );
 };
 
 export default LoginForm;
